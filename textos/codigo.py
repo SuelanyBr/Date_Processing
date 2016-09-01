f5a860b8b445fb4547985590f6378e9ffa76574b
SuelanyBr <maria.cruz@ccc.ufcg.edu.br>
2016-09-01 15:52:29 -0300

    adicao de minhas stopWords

--- textos/codigo.py
+++ textos/codigo.py
@@ -2,7 +2,9 @@
 # -*- coding: utf-8 -*-
 
 from os import path
-from wordcloud import WordCloud
+#from wordcloud import WordCloud
+import matplotlib.pyplot as plt
+import numpy as np
 
 # Obtem o camilho do diretorio que possui o arquivo procurado
 filepath = path.dirname(__file__)
@@ -11,6 +13,10 @@ filepath = path.dirname(__file__)
 text1 = open(path.join(filepath, 'Temer.txt')).read()
 text2 = open(path.join(filepath, 'Dilma.txt')).read()
 
+myStopwords = ['a', 'e', 'o', 'as', 'os', 'da', 'de', 'do', 'das', 'dos', 'um', 'uma', 'ele', 'ela', 'desse', 'dessa',
+	'na', 'no',	'esse', 'essa', 'mesmo', 'mesma', 'nesta', 'neste', 'essa', 'esse', 'disse', 'que', 'em', 'onde', 'aonde',
+	'fez', 'por', 'mas', 'foi', 'nao', 'com', 'para', 'isso', 'me', 'qual', 'tambem', 'outras', 'sem', 'auto', 'isso',
+	'isto', 'ira', 'se', 'logo', 'ja', 'tudo', 'todo', 'todos', 'muito', 'muita', 'pouco', 'mais', 'menos', 'quanto', 'ainda', 'antes' ]
 
 print text1
 print text2
